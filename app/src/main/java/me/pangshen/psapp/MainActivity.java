4e6311bdb6789b65eeb5e4d82a677ab3142b5d08
andy163 <ps_andy@163.com>
2016-01-12 18:09:10 +0800

    PANGSHEN

--- app/src/main/java/me/pangshen/psapp/MainActivity.java
+++ app/src/main/java/me/pangshen/psapp/MainActivity.java
@@ -3,6 +3,9 @@ package me.pangshen.psapp;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,14 +15,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private RecyclerView recyclerView;
+    private List<String> data = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        for (int i = 0; i < 100; i++) {
+            data.add("test" + i);
+        }
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayout.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(new MyAdapter(data));
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -97,5 +116,45 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+        List<String> data;
+
+        public MyAdapter(List<String> data) {
+            this.data = data;
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return super.getItemViewType(position);
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemLayout = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.content_main_item, null);
+            return new ViewHolder(itemLayout, 0);
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.tx.setText(data.get(position));
+
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            TextView tx;
+
+            public ViewHolder(View itemView, int itemType) {
+                super(itemView);
+                tx = (TextView) itemView.findViewById(R.id.text);
+            }
+        }
     }
 }
